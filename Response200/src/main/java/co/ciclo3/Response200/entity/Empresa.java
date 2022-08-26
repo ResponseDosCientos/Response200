2791d6011e5c70abe4701b1e0f8966d2ce7f7c84
samuelk14 <smho-rey14@hotmail.com>
2022-08-26 13:57:35 -0500

    Nuevo repo, agregue lo realizado por luis

---
+++ Response200/src/main/java/co/ciclo3/Response200/entity/Empresa.java
@@ -0,0 +1,50 @@
+package co.ciclo3.Response200.entity;
+
+public class Empresa {
+    private long nitEmpresa;
+    private String nomEmpresa;
+    private String dirEmpresa;
+    private int telEmpresa;
+
+    public long getNitEmpresa() {
+        return nitEmpresa;
+    }
+
+    public void setNitEmpresa(long nitEmpresa) {
+        this.nitEmpresa = nitEmpresa;
+    }
+
+    public String getNomEmpresa() {
+        return nomEmpresa;
+    }
+
+    public void setNomEmpresa(String nomEmpresa) {
+        this.nomEmpresa = nomEmpresa;
+    }
+
+    public String getDirEmpresa() {
+        return dirEmpresa;
+    }
+
+    public void setDirEmpresa(String dirEmpresa) {
+        this.dirEmpresa = dirEmpresa;
+    }
+
+    public int getTelEmpresa() {
+        return telEmpresa;
+    }
+
+    public void setTelEmpresa(int telEmpresa) {
+        this.telEmpresa = telEmpresa;
+    }
+
+    @Override
+    public String toString() {
+        return "Empresa{" +
+                "nitEmpresa=" + nitEmpresa +
+                ", nomEmpresa='" + nomEmpresa + '\'' +
+                ", dirEmpresa='" + dirEmpresa + '\'' +
+                ", telEmpresa=" + telEmpresa +
+                '}';
+    }
+}
