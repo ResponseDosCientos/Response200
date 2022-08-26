16eff9fe0acb8d3b792f07d4cbe583e9c873b904
cransters <108357630+cransters@users.noreply.github.com>
2022-08-25 22:48:54 -0500

    Primera prueba

---
+++ Response200/src/main/java/co/ciclo3/Response200/Response200Application.java
@@ -0,0 +1,13 @@
+package co.ciclo3.Response200;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Response200Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Response200Application.class, args);
+	}
+
+}
